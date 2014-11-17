f4e2badef636b9a36ccd581b4295fa2106acef70
Teacher <user@UHEVER-MAC.local>
2014-11-17 13:58:36 -0800

    Initial Commit

---
+++ Assignment_2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment_2
+//
+//  Created by Teacher on 14-11-17.
+//  Copyright (c) 2014年 UHEVER. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
